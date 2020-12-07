e7ee12758ac11139c31f06ad6f04518c752324ab
josselyne <USUARIO@DESKTOP-KOEI58B>
2020-12-06 22:37:37 -0500

    finalizacion del proyecto

--- src/salario/Salario.java
+++ src/salario/Salario.java
@@ -7,6 +7,8 @@ una de las primeras 40 horas y un aumento del 20% por cada hora extra.
  */
 package salario;
 
+import java.util.Scanner;
+
 /**
  *
  * josselyne ester chilito galindez cod: 20201187434
@@ -17,7 +19,24 @@ public class Salario {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner entrada=  new Scanner(System.in);
+        int horas,totalpagar,horasex;
+        System.out.println("ingre sus horas trabajas");
+        horas=entrada.nextInt();
+        
+        if(horas>=40){
+            totalpagar=40*5000+(horas-40)*6000;
+            /*100 es el 20%
+            1000+5000=6000
+            */
+            
+        }
+        else {
+           totalpagar=horas*5000;
+          
+        }
+           
+        System.out.println("su salario seria  :" + totalpagar);
     }
     
 }
