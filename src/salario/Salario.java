3500cc2509aa91f125674632147f856b6f8afeca
josselyne <>
2020-12-06 21:56:05 -0500

    inicio de proyecto

---
+++ src/salario/Salario.java
@@ -0,0 +1,23 @@
+/*
+ *Un obrero necesita calcular su salario semanal, 
+el cual se obtiene de la sig. manera:
+•Si trabaja 40 horas o menos se le paga $5000 por hora
+•Si trabaja más de 40 horas se le paga $5000 por cada 
+una de las primeras 40 horas y un aumento del 20% por cada hora extra.
+ */
+package salario;
+
+/**
+ *
+ * josselyne ester chilito galindez cod: 20201187434
+ */
+public class Salario {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
